d90af225c8fd0e17c6a215bcbb0f3ffd75b7a444
Harish Sunkerikar <urhari04@gmail.com>
2020-01-19 11:22:34 +0530

    Minify JS, CSS, and HTML in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,12 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,11 +2,44 @@ const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssestsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = merge(common, {
   mode: "production",
-  plugins: [new CleanWebpackPlugin()],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contentHash].bundle.js"
+  },
+  //This line is added just to show how to optimize css , Sass-loader actually minifies css now by default
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssestsPlugin(),
+      new TerserWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true
+        }
+      })
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contentHash].css"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      }
+    ]
   }
 });
